9ea553c07373c6ab0b052388b1bda3be76fa9685
JunayedRashid56 <junayed.isha37@gmail.com>
2026-05-09 02:22:38 +0600

    added js file contents

--- Mid/Lab3/Javascript/script.js
+++ Mid/Lab3/Javascript/script.js
@@ -1,1 +1,38 @@
 console.log("Connected");
+
+function collet_data() {
+    let isNameValid = collect_name();
+    let isAgeValid =collect_age();
+
+    return false;
+}
+
+function collect_name(){
+    let name = document.getElementById("PatientName").value;
+    if(name==""){
+        document.getElementById("NameError").innerHTML="Name can't be Empty";
+        return false;
+    }
+    else if(name.length < 5){
+        document.getElementById("NameError").innerHTML="Name must consist atleast 5 characters";
+        return false;
+    }
+    else{
+        console.log(name);
+    }
+    return false;
+}
+
+function collect_age()
+{
+    let age = document.getElementById("Age").value;
+    if(age == "")
+    {
+        document.getElementById("AgeError").innerHTML="Age Can not be Empty";
+        return false;
+    }
+    else{
+        console.log(age);
+    }
+    return false;
+}
